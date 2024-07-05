b0d4131686725c43b35bb0280de5e945a3255d82
nathaniel-li-legend <nathaniel.li@legendfleet.com>
2024-07-05 07:25:34 -0400

    created user getter

--- app.py
+++ app.py
@@ -8,12 +8,9 @@ def hello_world():
         
     # To test the get_test_user_group function
     user_groups = Getters.get_users()
-
     print(user_groups)
     
     return "Hello World!"
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)
