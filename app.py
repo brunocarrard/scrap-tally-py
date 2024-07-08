20b5ba8f0e82f96bb3b7b22d46f69461cc57ef9e
Bruno Carrard <brunocarrardoliveira@gmail.com>
2024-07-08 15:47:23 -0300

    changed Hello World return to user_groups and add CORS validation

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
 from flask import Flask
 from controllers.getters import Getters
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app)
 @app.route('/')
 def hello_world():
-        
     # To test the get_test_user_group function
     user_groups = Getters.get_users()
-    print(user_groups)
-    
-    return "Hello World!"
+    return user_groups
 
 if __name__ == '__main__':
     app.run(debug=True)
